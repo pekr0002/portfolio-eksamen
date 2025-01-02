526423e665c479fb101f177066d0c168576f95e1
Penny <pekr0002@stud.kea.dk>
2025-01-02 12:15:47 +0100

    tilføjet billede showcase på index

---
+++ js/carousel.js
@@ -0,0 +1,35 @@
+// skabelon fundet på https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
+// variabel "slideIndex" bliver sat til 1
+let slideIndex = 1;
+// showSlides bliver sat til at være =1
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  console.log("siden vises");
+  let i;
+  // der bliver lavet en liste ud fra "mySlides" klassen
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
