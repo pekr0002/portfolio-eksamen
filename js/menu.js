73a3698bc1ec41eb0724f92dc756148ea61b35cf
Penny <pekr0002@stud.kea.dk>
2025-01-07 01:47:35 +0100

    Kommentarer i index.html og menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,16 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
-
+// "burger" lytter efter klik
 burger.addEventListener("click", burgerClick);
+//"burgerClick" bliver kaldt
 function burgerClick() {
+  // burger får "active" klassen
   burger.classList.toggle("active");
+  // nav får "active" klassen
   nav.classList.toggle("active");
 }
+// når menuen bliver lukket igen bliver "active" fjernet fra "burger" og "nav"
 menu.addEventListener("click", menuClick);
 function menuClick() {
   burger.classList.remove("active");
