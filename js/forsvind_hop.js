02d434609ee1a0988b3d328dd064108d7c698b3b
Penny <pekr0002@stud.kea.dk>
2025-01-07 01:32:20 +0100

    kommentarer i carousel.css, layout.css, nav.css og forsvind_hop.js

--- js/forsvind_hop.js
+++ js/forsvind_hop.js
@@ -1,16 +1,21 @@
+// siden vises
 window.addEventListener("load", sidenVises);
 function sidenVises() {
   console.log("siden vises");
-  document.querySelector("#purple_container").addEventListener("click", clickRedHandler);
+  //  lytter efter når der bliver klikket på "purple_container"
+  document.querySelector("#purple_container").addEventListener("click", clickPurpleHandler);
 }
 
-function clickRedHandler() {
-  console.log("clickredhandler");
+function clickPurpleHandler() {
+  console.log("clickPurplehandler");
+  // "purple_sprite" får klassen "forsvind"
   document.querySelector("#purple_sprite").classList.add("forsvind");
+  // når animationen for "forsvind" er færdig bliver "forsvindDone" kaldt
   document.querySelector("#purple_container").addEventListener("animationend", forsvindDone);
 }
 
 function forsvindDone() {
   console.log("forsvinddone");
+  // "green_sprite" får klassen hop
   document.querySelector("#green_sprite").classList.add("hop");
 }
