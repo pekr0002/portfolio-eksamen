e2c72bc82d3187825402780117d8de578fd01bb3
Penny <pekr0002@stud.kea.dk>
2025-01-07 01:02:12 +0100

    billede karusel opdateret

--- js/carousel.js
+++ js/carousel.js
@@ -1,35 +1,57 @@
 // skabelon fundet på https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
-// variabel "slideIndex" bliver sat til 1
-let slideIndex = 1;
-// showSlides bliver sat til at være =1
-showSlides(slideIndex);
+// variabel "slideIndex" bliver opretter og sat til 1
+let imageIndex = 1;
+// showImages bliver sat til at være =1
+showImages(imageIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+// funktionen bliver kaldt når der bliver klikket på næste billede
+function plusImages(n) {
+  showImages((imageIndex += n));
 }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+//funktionen bliver kaldt når det bliver klikket på en af prikkerne
+function currentImage(n) {
+  showImages((imageIndex = n));
 }
+// funktionen bliver kaldt når der skal blive skiftet billede
+function showImages(n) {
+  console.log("billede", n);
 
-function showSlides(n) {
-  console.log("siden vises");
+  //   variablen "i" bliver oprettet
   let i;
-  // der bliver lavet en liste ud fra "mySlides" klassen
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+  // der bliver lavet en liste ud fra "myImages" klassen
+  let images = document.getElementsByClassName("myImages");
+
+  // der bliver lavet en liste ud fra "prik" klassen
+  let prikker = document.getElementsByClassName("prik");
+
+  //   hvis n er større end "images" længden bliver imageIndex sat til 1
+  if (n > images.length) {
+    imageIndex = 1;
   }
+  //   hvis n er mindre end 1 bliver imageIndex sat til at være det samme som "images" længden
   if (n < 1) {
-    slideIndex = slides.length;
+    imageIndex = images.length;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+
+  //   Variablen i sættes til 0.
+  //   Løkken fortsætter, så længe "i" er mindre end længden af images-listen.
+  //   Efter hver iteration øges i med 1.
+  for (i = 0; i < images.length; i++) {
+    console.log(i);
+    // images listen bliver sat til "display:none;"
+    images[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  //   Variablen i sættes til 0.
+  //   Løkken fortsætter, så længe "i" er mindre end længden af prikker-listen.
+  //   Efter hver iteration øges i med 1.
+  for (i = 0; i < prikker.length; i++) {
+    // alle i "prikker" listen får fjernet "active" klassen
+    prikker[i].className = prikker[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  // det aktuelle billede bliver sat til "display:block;"
+  images[imageIndex - 1].style.display = "block";
+  //   prik som svare til siden for klassen "active"
+  prikker[imageIndex - 1].className += " active";
 }
